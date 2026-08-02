7aa069534f3754cc7c83bb69f2b005a5cb832363
Anonymous 🎭 <157519745+anonymous-beta@users.noreply.github.com>
2026-08-02 12:47:32 +0100

    Initialize webfly module with version and author info

---
+++ webfly/__init__.py
@@ -0,0 +1,7 @@
+"""
+WebFly — Aggressive Web Reconnaissance & Exploitation Framework
+Created by Anonymous-beta & Victor410fer
+"""
+
+__version__ = "1.0.0"
+__author__ = "Anonymous-beta & Victor410fer"
